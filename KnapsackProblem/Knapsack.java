a5f985d0ffae99ce0829188223c91a7cf8231350
David Tigre Moraes <tigredavid007@gmail.com>
2023-08-09 00:31:07 -0300

    DC and DP in Knapsack Problem

--- KnapsackProblem/Knapsack.java
+++ KnapsackProblem/Knapsack.java
@@ -1,30 +1,73 @@
 package KnapsackProblem;
 
+import java.text.MessageFormat;
 import java.util.*;
 import Contracts.IExecutable;
+import Conversion.Conversion;
+import Generators.KnapsackGenerator;
 import TimeCalculation.*;
 
 public class Knapsack implements IExecutable {
+    private List<List<ItemKnapsack>> itens = new ArrayList<List<ItemKnapsack>>();
+
     private List<CalculationResult> resultsDefault = new ArrayList<CalculationResult>();
     private List<CalculationResult> resultsDivideConquer = new ArrayList<CalculationResult>();
     private List<CalculationResult> resultsDynamicProg = new ArrayList<CalculationResult>();
 
     private TimeCalculation timeCalculation = new TimeCalculation();
 
+    public Knapsack() {
+        super();
+        itens.add(KnapsackGenerator.GenerateRandomList(50));
+        itens.add(KnapsackGenerator.GenerateRandomList(75));
+        itens.add(KnapsackGenerator.GenerateRandomList(100));
+        itens.add(KnapsackGenerator.GenerateRandomList(200));
+    }
+
     @Override
     public void ExecuteAllMethods() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ExecuteAllMethods'");
+        ExecuteKnapsackDefault();
+        ExecuteKnapsackDynamicProgramming();
+        //ExecuteKnapsackDivideConquer();
     }
 
     @Override
     public void ShowResults() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'ShowResults'");
+        System.out.println("\n\nRESULTADOS DEFAULT\n");
+        PrintResults(resultsDefault);
+
+        System.out.println("\n\nRESULTADOS DIVISÃO E CONQUISTA\n");
+        PrintResults(resultsDivideConquer);
+
+        System.out.println("\n\nRESULTADOS PROGRAMAÇÃO DINAMICA\n");
+        PrintResults(resultsDynamicProg);
+    }
+
+    private void PrintResults(List<CalculationResult> results) {
+        int i = 0;
+        for (CalculationResult result : resultsDefault) {
+            long resultadoOperacao = Conversion.toLong(result.getResult());
+            double duracaoEmMs = result.getTimeDuration();
+
+            System.out.println(MessageFormat.format("Duração: {0}", duracaoEmMs));
+            System.out.println(
+                    MessageFormat.format("KNAPSACK ({0} ELEMENTOS) => {1} ", itens.get(i).size(), resultadoOperacao));
+            
+            i++;
+        }
     }
 
     private void ExecuteKnapsackDefault() {
-        
+        for (int i = 0; i < itens.size(); i++) {
+            String[] params = { Integer.toString(i) };
+            List<ItemKnapsack> copyItens = new ArrayList<ItemKnapsack>(itens.get(i));
+
+            CalculationResult resultado = timeCalculation.CalculateTimeConsuming(args -> {
+                return KnapsackDefault(copyItens, 1000);
+            }, params);
+
+            resultsDefault.add(resultado);
+        }
     }
 
     private int KnapsackDefault(List<ItemKnapsack> itens, int maxWeight) {
@@ -44,6 +87,69 @@ public class Knapsack implements IExecutable {
         return retornaMaior(
                 lastItemValue + KnapsackDefault(itens, maxWeight - lastItemWeight),
                 KnapsackDefault(itens, maxWeight));
+    }
+
+    private void ExecuteKnapsackDivideConquer() {
+        for (int i = 0; i < itens.size(); i++) {
+            String[] params = { Integer.toString(i) };
+            List<ItemKnapsack> copyItens = new ArrayList<ItemKnapsack>(itens.get(i));
+
+            CalculationResult resultado = timeCalculation.CalculateTimeConsuming(args -> {
+                return KnapsackDivideConquer(copyItens, 1000, copyItens.size() - 1);
+            }, params);
+
+            resultsDivideConquer.add(resultado);
+        }
+    }
+
+    private int KnapsackDivideConquer(List<ItemKnapsack> itens, int maxWeight, int index) {
+        if (index < 0 || maxWeight <= 0) {
+            return 0;
+        }
+
+        int pesoDoIndex = itens.get(index).getWeight();
+        int valorDoIndex = itens.get(index).getValue();
+
+        if (pesoDoIndex > maxWeight) {
+            return KnapsackDivideConquer(itens, maxWeight, index - 1);
+        } else {
+            int withItem = valorDoIndex + KnapsackDivideConquer(itens, maxWeight - pesoDoIndex, index - 1);
+            int withoutItem = KnapsackDivideConquer(itens, maxWeight, index - 1);
+            return Math.max(withItem, withoutItem);
+        }
+    }
+
+    private void ExecuteKnapsackDynamicProgramming() {
+        for (int i = 0; i < itens.size(); i++) {
+            String[] params = { Integer.toString(i) };
+            List<ItemKnapsack> copyItens = new ArrayList<ItemKnapsack>(itens.get(i));
+
+            CalculationResult resultado = timeCalculation.CalculateTimeConsuming(args -> {
+                return KnapsackDynamicProgramming(copyItens, 1000);
+            }, params);
+
+            resultsDynamicProg.add(resultado);
+        }
+    }
+
+    private int KnapsackDynamicProgramming(List<ItemKnapsack> itens, int maxWeight) {
+        int n = itens.size();
+        int[][] memo = new int[n + 1][maxWeight + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 0; w <= maxWeight; w++) {
+                int pesoIndex = itens.get(i - 1).getWeight();
+                int valorIndex = itens.get(i - 1).getValue();
+
+                if (pesoIndex > w) {
+                    memo[i][w] = memo[i - 1][w];
+                } else {
+                    memo[i][w] = Math.max(memo[i - 1][w], valorIndex + memo[i - 1][w - pesoIndex]);
+                }
+            }
+        }
+
+        return memo[n][maxWeight];
     }
 
     private int retornaMaior(int a, int b) {
--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 import Exponentiation.Exponentiation;
+import KnapsackProblem.Knapsack;
 import SubArrayProblem.MaximumSubarray;
 
 public class Main {
     public static void main (String[] args) {
         Exponentiation exponentiation = new Exponentiation();
         MaximumSubarray maximumSubarray = new MaximumSubarray();
+        Knapsack knapsackProblem = new Knapsack();
+
+        knapsackProblem.ExecuteAllMethods();
+        knapsackProblem.ShowResults();
 
         exponentiation.ExecuteAllMethods();
         exponentiation.ShowResults();
 
         maximumSubarray.ExecuteAllMethods();
         maximumSubarray.ShowResults();
+
     }
 }
